0a5ef595aee4864b1086c60a587c190e815f6dae
MYKOLA SHEREMET <artem.vazovskin@gmail.com>
2022-09-10 16:41:25 +0300

    make task component 1

--- src/index.js
+++ src/index.js
@@ -8,3 +8,47 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
+
+// const element = React.createElement('div', { class: "new", type:"null", children: "Привет" });
+
+// ReactDOM.render(element, document.querySelector('#root'));
+
+// const div = document.querySelector('#root');
+
+
+// console.log(div);
+
+// function OurProfile() {
+//   return (<div class="profile">
+//   <div class="description">
+//     <img
+//       src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+//       alt="User avatar"
+//       class="avatar"
+//     />
+//     <p class="name">Petra Marica</p>
+//     <p class="tag">@pmarica</p>
+//     <p class="location">Salvador, Brasil</p>
+//   </div>
+
+//   <ul class="stats">
+//     <li>
+//       <span class="label">Followers</span>
+//       <span class="quantity">1000</span>
+//     </li>
+//     <li>
+//       <span class="label">Views</span>
+//       <span class="quantity">2000</span>
+//     </li>
+//     <li>
+//       <span class="label">Likes</span>
+//       <span class="quantity">3000</span>
+//     </li>
+//   </ul>
+// </div>)
+  
+// };
+
+
+// ReactDOM.render(<OurProfile/>, document.querySelector('#root'));
+
